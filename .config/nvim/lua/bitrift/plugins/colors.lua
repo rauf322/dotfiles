aa80e96e84eafda4cfe5226dac4177930e350c93
rauf322 <rauf.faizov.200271@gmail.com>
2025-08-25 22:46:45 +0200

    Autocommit

--- .config/nvim/lua/bitrift/plugins/colors.lua
+++ .config/nvim/lua/bitrift/plugins/colors.lua
@@ -20,14 +20,6 @@ return {
 	},
 
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		opts = {},
-		config = function()
-			ColorMyPencils()
-		end,
-	},
-	{
 		"ellisonleao/gruvbox.nvim",
 		name = "gruvbox",
 		config = function()
@@ -61,8 +53,6 @@ return {
 		"folke/tokyonight.nvim",
 		config = function()
 			require("tokyonight").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
 				style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
 				transparent = true, -- Enable this to disable setting the background color
 				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
@@ -78,7 +68,6 @@ return {
 			})
 		end,
 	},
-
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
