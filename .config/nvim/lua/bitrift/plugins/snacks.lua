9e947faf17621e19ea4fe862b67b3668b8f0a901
rauf322 <rauf.faizov.200271@gmail.com>
2025-09-01 09:46:50 +0200

    add folke/flash

--- .config/nvim/lua/bitrift/plugins/snacks.lua
+++ .config/nvim/lua/bitrift/plugins/snacks.lua
@@ -66,27 +66,24 @@ return {
 		terminal = {
 			start_insert = false,
 			auto_insert = false,
-			-- keep terminal open when process exits
 			auto_close = false,
 			win = {
-				-- border = "rounded",
 				style = "minimal",
 			},
-			-- in your snacks.lua or keymaps.lua
-			vim.keymap.set("n", "<leader>`", function()
-				require("snacks.terminal").toggle()
-			end, { desc = "Toggle terminal" }),
-			vim.keymap.set("n", "<leader>tg", function()
-				require("snacks.terminal").toggle("lazygit")
-			end, { desc = "Toggle lazygit" }),
-			vim.keymap.set("n", "<leader>th", function()
-				require("snacks.terminal").toggle("btop")
-			end, { desc = "Toggle htop" }),
 		},
 	},
 
 	config = function(_, opts)
 		require("snacks").setup(opts)
+		vim.keymap.set("n", "<leader>`", function()
+			require("snacks.terminal").toggle()
+		end, { desc = "Toggle terminal" })
+		vim.keymap.set("n", "<leader>tg", function()
+			require("snacks.terminal").toggle("lazygit")
+		end, { desc = "Toggle lazygit" })
+		vim.keymap.set("n", "<leader>th", function()
+			require("snacks.terminal").toggle("btop")
+		end, { desc = "Toggle htop" })
 
 		vim.o.ttimeoutlen = 0
 	end,
