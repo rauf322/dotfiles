627409f2a1cffdf61dd9c2dd8cfc79e046a55306
rauf322 <rauf.faizov.200271@gmail.com>
2025-10-26 17:30:20 +0100

    nvim 0.12

--- .config/nvim/lua/bitrift/lsp/sqls.lua
+++
@@ -1,5 +0,0 @@
-return {
-	cmd = { "sqls" },
-	filetypes = { "sql", "mysql", "pgsql" },
-	root_markers = { ".git" },
-}
--- .config/nvim/lua/bitrift/lspconfig.lua
+++ .config/nvim/lua/bitrift/lspconfig.lua
@@ -1,78 +1,58 @@
--- lsp
---------------------------------------------------------------------------------
--- See https://gpanders.com/blog/whats-new-in-neovim-0-11/ for a nice overview
--- of how the lsp setup works in neovim 0.11+.
+local keymap = vim.keymap
 
--- This actually just enables the lsp servers.
--- The configuration is found in the lsp folder inside the nvim config folder,
--- so in ~.config/lsp/lua_ls.lua for lua_ls, for example.
-local lsp_servers = {
-	"lua_ls",
-	"cssls",
-	"tailwindcss",
-	"svelte",
-	"graphql",
-	"jsonls",
-	"emmet_language_server",
-	"prismals",
-	"dockerls",
-	"sqls",
-	"yamlls",
-}
-for _, server in ipairs(lsp_servers) do
-	vim.lsp.enable(server)
-end
+-- Disabled inline completion (ghost text) - using nvim-cmp instead
+-- vim.lsp.inline_completion.enable()
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
-		local opts = { buffer = ev.buf, silent = true }
+		local buffer = ev.buf
+		local opts = { buffer = buffer, silent = true }
 
-		-- keymaps
+		-- Standard LSP keymaps
 		opts.desc = "Show LSP references"
-		vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+		keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
 		opts.desc = "Go to declaration"
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+		keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
 		opts.desc = "Show LSP definitions"
-		vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+		keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
 		opts.desc = "Show LSP implementations"
-		vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+		keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
 		opts.desc = "Show LSP type definitions"
-		vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+		keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
 		opts.desc = "See available code actions"
-		vim.keymap.set({ "n", "v" }, "<leader>ca", function()
+		keymap.set({ "n", "v" }, "<leader>ca", function()
 			vim.lsp.buf.code_action()
-		end, opts) -- see available code actions, in visual mode will apply to selection
-
-		opts.desc = "Smart rename"
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+		end, opts)
 
 		opts.desc = "Show buffer diagnostics"
-		vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+		keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
 		opts.desc = "Show all diagnostics"
-		vim.keymap.set("n", "<leader>DA", "<cmd>Telescope diagnostics<CR>", opts) -- show diagnostics from all buffers
+		keymap.set("n", "<leader>DA", "<cmd>Telescope diagnostics<CR>", opts)
 
 		opts.desc = "Show line diagnostics"
-		vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+		keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
 		opts.desc = "Show definiton of word (type)"
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+		keymap.set("n", "K", vim.lsp.buf.hover, opts)
 	end,
 })
 
--- Diagnostics
-vim.diagnostic.config({
-	-- Use the default configuration
-	-- virtual_lines = true
+local severity = vim.diagnostic.severity
 
-	-- Alternatively, customize specific options
-	virtual_lines = {
-		-- Only show virtual line diagnostics for the current cursor line
-		current_line = true,
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[severity.ERROR] = " ",
+			[severity.WARN] = " ",
+			[severity.HINT] = "󰠠 ",
+			[severity.INFO] = " ",
+		},
 	},
 })
