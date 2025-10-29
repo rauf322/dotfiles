f3c5404de8af1537e397f06df465cc3aa1fb3a58
rauf322 <rauf.faizov.200271@gmail.com>
2025-10-29 16:13:53 +0100

    added bg black

--- .config/nvim/lua/bitrift/plugins/lualine.lua
+++ .config/nvim/lua/bitrift/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
 		lualine.setup({
 			options = {
 				icons_enabled = true,
-				theme = "palenight",
+				theme = "auto",
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "|", right = "|" },
 				disabled_filetypes = {},
